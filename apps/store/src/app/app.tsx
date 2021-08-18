fc5ecb8db2592abfe2dc13b704da6e87a27a2638
John Soter <john.soter@outlook.com>
2021-08-18 14:01:21 -0700

    change title

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -49,7 +49,7 @@ export const App = () => {
 
   return (
     <>
-      <Header title="Board Game Hoard John" />
+      <Header title="Board Game Hoard" />
       <div className={styles.container}>
         <div className={styles['games-layout']}>
           {state.loadingState === 'loading'
